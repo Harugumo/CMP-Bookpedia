88de342cb08f7e38fd8313dd1fd352d09cab89aa
Harugumo <qperret@orange.fr>
2025-03-08 08:19:05 +0100

    add: BlurredImageBackground component

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -0,0 +1,43 @@
+package com.plcoding.bookpedia.book.presentation.book_detail
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.bookpedia.book.presentation.book_detail.components.BlurredImageBackground
+
+@Composable
+fun BookDetailScreenRoot(
+    viewModel: BookDetailViewModel,
+    onBackClick: () -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookDetailScreen(
+        state,
+        onAction = { action ->
+            when (action) {
+                is IBookDetailAction.OnBackClick -> onBackClick()
+                else -> Unit
+            }
+            viewModel.onAction(action)
+        }
+    )
+}
+
+@Composable
+private fun BookDetailScreen(
+    state: BookDetailState,
+    onAction: (IBookDetailAction) -> Unit
+) {
+    BlurredImageBackground(
+        imageUrl = state.book?.imageUrl,
+        isFavorite = state.isFavorite,
+        onFavoriteClick = { onAction(IBookDetailAction.OnFavoriteClick) },
+        onBackClick = { onAction(IBookDetailAction.OnBackClick) },
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
